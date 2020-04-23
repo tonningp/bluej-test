133d4761081442f947d4536ab70a08288a1e3065
Paul Tonning <paul.tonning@student.vvc.edu>
2020-04-23 12:29:33 -0700

    update

---
+++ CashRegister.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class CashRegister here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CashRegister
+{
+
+
+    /**
+     * Constructor for objects of class CashRegister
+     */
+    public CashRegister()
+    {
+       
+    }
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String args[])
+    {
+       CashRegister r = new CashRegister();
+       
+       System.out.println("This is a test");
+    }
+}
