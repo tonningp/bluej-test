8f15195422b9431213e2e93e1d99b2b918d3cd5d
Paul Tonning <paul.tonning@student.vvc.edu>
2020-04-23 12:44:16 -0700

    update

--- CashRegister.java
+++ CashRegister.java
@@ -5,16 +5,19 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+
+import java.util.ArrayList;
+
 public class CashRegister
 {
-
+    private ArrayList<Integer> transactions;
 
     /**
      * Constructor for objects of class CashRegister
      */
     public CashRegister()
     {
-       
+       transactions = new ArrayList<Integer>();
     }
 
     
